f2ba12cb10f00e8fa339b6cfb756e83835b5c718
dbes-gds <daniel.besbrode@digital.cabinet-office.gov.uk>
2021-08-04 15:54:44 +0100

    BAU: add scenario to check legal and policy page links

---
+++ acceptance-tests/src/test/java/uk/gov/di/test/acceptance/LegalAndPolicyPagesStepDefinitions.java
@@ -0,0 +1,73 @@
+package uk.gov.di.test.acceptance;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LegalAndPolicyPagesStepDefinitions extends SignInStepDefinitions {
+
+    @Before
+    public void setupWebdriver() throws MalformedURLException {
+        super.setupWebdriver();
+    }
+
+    @After
+    public void closeWebdriver() {
+        super.closeWebdriver();
+    }
+
+    @Given("the services are running")
+    public void theServicesAreRunning() {
+    }
+
+    @When("the user visits the stub relying party")
+    public void theExistingUserVisitsTheStubRelyingParty() {
+        driver.get(RP_URL.toString());
+    }
+
+    @And("the user clicks {string}")
+    public void theExistingUserClicks(String buttonName) {
+        WebElement button = driver.findElement(By.id(buttonName));
+        button.click();
+    }
+
+    @Then("the user is taken to the Identity Provider Login Page")
+    public void theExistingUserIsTakenToTheIdentityProviderLoginPage() {
+        waitForPageLoad("Sign in to or create your GOV.UK Account");
+        assertEquals("/enter-email", URI.create(driver.getCurrentUrl()).getPath());
+        assertEquals(IDP_URL.getHost(), URI.create(driver.getCurrentUrl()).getHost());
+        assertEquals("Sign in to or create your GOV.UK Account - GOV.UK Account", driver.getTitle());
+    }
+
+    @And("the user clicks link {string}")
+    public void theUserClicksLink(String linkText) {
+        driver.findElement(By.partialLinkText(linkText)).click();
+    }
+
+    @Then("the user is taken to the page with title containing {string}")
+    public void theUserIsTakenToThePageWithTitleContaining(String title) {
+        waitForPageLoad(title);
+    }
+
+    @And("the user navigates back")
+    public void theUserNavigatesBack() {
+        driver.navigate().back();
+    }
+
+    private void waitForPageLoad(String titleContains) {
+        new WebDriverWait(driver, DEFAULT_PAGE_LOAD_WAIT_TIME).until(ExpectedConditions.titleContains(titleContains));
+    }
+}
