65bc2dc9463b2e759ed38e4080b60a2c6b778708
dbes-gds <daniel.besbrode@digital.cabinet-office.gov.uk>
2021-11-01 17:15:07 +0000

    BAU: fix update password by maintaining the session between scenarios

--- acceptance-tests/src/test/java/uk/gov/di/test/acceptance/LegalAndPolicyPagesStepDefinitions.java
+++ acceptance-tests/src/test/java/uk/gov/di/test/acceptance/LegalAndPolicyPagesStepDefinitions.java
@@ -1,18 +1,17 @@
 package uk.gov.di.test.acceptance;
 
-import static org.junit.Assert.assertEquals;
-
-import java.net.MalformedURLException;
-import java.net.URI;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-
 import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+
+import static org.junit.Assert.assertEquals;
 
 public class LegalAndPolicyPagesStepDefinitions extends SignInStepDefinitions {
 
@@ -21,7 +20,6 @@ public class LegalAndPolicyPagesStepDefinitions extends SignInStepDefinitions {
         super.setupWebdriver();
     }
 
-    
     @Given("the services are running")
     public void theServicesAreRunning() {}
 
