f3ec4928fd0f3ff12141b915193f54300ef01557
dbes-gds <daniel.besbrode@digital.cabinet-office.gov.uk>
2023-02-27 15:58:59 +0000

    AUT-1068: Reset passwords for reset password test users

--- reset-test-data.sh
+++ reset-test-data.sh
@@ -102,3 +102,22 @@ gds-cli aws ${GDS_AWS_ACCOUNT} aws dynamodb update-item \
     --expression-attribute-values "{ \":vtc\": { \"M\": { \"version\": { \"S\": \"1.0\"}, \"timestamp\": {\"S\": \"1970-01-01T00:00:00.000000\"} } } }" \
     --region "${AWS_REGION}"
 
+echo "resetting password for reset password test user in user-credentials..."
+export HASHED_PASSWORD_RESET=$(echo -n "${RESET_PW_CURRENT_PW}" | argon2 $(openssl rand -hex 32) -e -id -v 13 -k 15360 -t 2 -p 1)
+gds-cli aws ${GDS_AWS_ACCOUNT} aws dynamodb update-item \
+    --table-name "${ENVIRONMENT_NAME}-user-credentials" \
+    --key "{\"Email\": {\"S\": \"${RESET_PW_USER_EMAIL}\"}}" \
+    --update-expression "SET #PW = :vpw" \
+    --expression-attribute-names "{ \"#PW\": \"Password\" }" \
+    --expression-attribute-values "{ \":vpw\": { \"S\": \"${HASHED_PASSWORD_RESET}\"} }" \
+    --region "${AWS_REGION}"
+
+echo "resetting password for require password reset test user in user-credentials..."
+export HASHED_PASSWORD_REQ_RESET=$(echo -n "${REQ_RESET_PW_CURRENT_PW}" | argon2 $(openssl rand -hex 32) -e -id -v 13 -k 15360 -t 2 -p 1)
+gds-cli aws ${GDS_AWS_ACCOUNT} aws dynamodb update-item \
+    --table-name "${ENVIRONMENT_NAME}-user-credentials" \
+    --key "{\"Email\": {\"S\": \"${REQ_RESET_PW_USER_EMAIL}\"}}" \
+    --update-expression "SET #PW = :vpw" \
+    --expression-attribute-names "{ \"#PW\": \"Password\" }" \
+    --expression-attribute-values "{ \":vpw\": { \"S\": \"${HASHED_PASSWORD_REQ_RESET}\"} }" \
+    --region "${AWS_REGION}"
