92f0a0c009a220af41022198fcfcb1bea308b0b4
Craig.Ryder <craig.ryder@digital.cabinet-office.gov.uk>
2023-03-07 11:38:28 +0000

    AUT-1132 - replaced Allure reporting with Cucumber reports

--- acceptance-tests/src/test/java/uk/gov/di/test/runners/RunCucumberTest.java
+++ acceptance-tests/src/test/java/uk/gov/di/test/runners/RunCucumberTest.java
@@ -5,5 +5,5 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber-report/index.html"})
 public class RunCucumberTest {}
