e55a6a8278e00d796b3d6b489e1d18aae257d153
dbes-gds <38218115+dbes-gds@users.noreply.github.com>
2023-04-17 16:33:25 +0100

    Revert "AUT-995: Page title changes for password reset"

--- acceptance-tests/src/test/java/uk/gov/di/test/utils/AuthenticationJourneyPages.java
+++ acceptance-tests/src/test/java/uk/gov/di/test/utils/AuthenticationJourneyPages.java
@@ -14,7 +14,8 @@ public enum AuthenticationJourneyPages {
             "/get-security-codes", "Finish creating your account"),
     SETUP_AUTHENTICATOR_APP("/setup-authenticator-app", "Set up an authenticator app"),
     ENTER_AUTHENTICATOR_APP_CODE(
-            "/enter-authenticator-app-code", "You need to enter a security code"),
+            "/enter-authenticator-app-code",
+            "You need to enter a security code"),
     ENTER_PHONE_NUMBER("/enter-phone-number", "Enter your mobile phone number"),
     FINISH_CREATING_YOUR_ACCOUNT("/enter-phone-number", "Finish creating your account"),
     CHECK_YOUR_PHONE("/check-your-phone", "Check your phone"),
