e54a8d4d279d6157bce9a1b55dd54caa7eff2491
Craig.Ryder <craig.ryder@digital.cabinet-office.gov.uk>
2023-06-12 13:58:50 +0100

    AUT-1336 - removed asterisk in the signin import files

--- acceptance-tests/src/test/java/uk/gov/di/test/utils/SignIn.java
+++ acceptance-tests/src/test/java/uk/gov/di/test/utils/SignIn.java
@@ -1,6 +1,8 @@
 package uk.gov.di.test.utils;
 
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
