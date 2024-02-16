a8b38a5858de9a88a1f57fede70ec089a06d99c4
dbes-gds <daniel.besbrode@digital.cabinet-office.gov.uk>
2024-02-16 17:20:16 +0000

    BAU: Ability to run acceptance tests locally against staging
    Make sure you have the correct environment variables then run the following command:
    
    ./run-acceptance-tests.sh -l staging

--- reset-test-data.sh
+++ reset-test-data.sh
@@ -23,17 +23,18 @@ while getopts "lr" opt; do
   esac
 done
 
-echo -e "Resetting di-authentication-acceptance-tests test data..."
+echo -e "Resetting di-authentication-acceptance-tests test data in ${ENVIRONMENT}..."
 
 export AWS_REGION=eu-west-2
 export ENVIRONMENT_NAME=$ENVIRONMENT
-export GDS_AWS_ACCOUNT=digital-identity-dev
 
 if [ $LOCAL == "1" ]; then
   export $(grep -v '^#' .env | xargs)
-fi
-
-if [ $LOCAL == "1" ]; then
+  if [ $ENVIRONMENT == "staging" ]; then
+    export GDS_AWS_ACCOUNT=di-auth-staging
+  else
+    export GDS_AWS_ACCOUNT=digital-identity-dev
+  fi
   echo -e "Getting AWS credentials ..."
   eval "$(gds aws ${GDS_AWS_ACCOUNT} -e)"
   echo "done!"
