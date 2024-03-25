282386124e544b4d57cf801f5a9f7480dc0d1d7a
dbes-gds <38218115+dbes-gds@users.noreply.github.com>
2024-03-25 17:26:29 +0000

    Revert "added Reauth tests"

--- acceptance-tests/src/test/java/uk/gov/di/test/pages/RpStubPage.java
+++ acceptance-tests/src/test/java/uk/gov/di/test/pages/RpStubPage.java
@@ -3,7 +3,6 @@ package uk.gov.di.test.pages;
 import org.openqa.selenium.By;
 
 public class RpStubPage extends BasePage {
-    By reauthIdTokenField = By.id("reauth-id-token");
 
     public void goToRpStub() {
         driver.get(RP_URL.toString());
@@ -18,17 +17,5 @@ public class RpStubPage extends BasePage {
             }
         }
         findAndClickContinue();
-    }
-
-    public void enterReauthIdToken(String token) {
-        driver.findElement(reauthIdTokenField).clear();
-        driver.findElement(reauthIdTokenField).sendKeys(token);
-    }
-
-    public void reauthRequired(String idToken) {
-        goToRpStub();
-        enterReauthIdToken(idToken);
-        selectRpOptionsByIdAndContinue("2fa-on,prompt-login,request-object");
-        setAnalyticsCookieTo(false);
     }
 }
