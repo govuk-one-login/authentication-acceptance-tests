6d2b822ed50b125300e350238dea9f14e2caf0f3
LazarAlexandruConstantin <alex.lazar@digital.cabinet-office.gov.uk>
2024-07-31 10:14:45 +0300

    AUT-3386: Retrieve access token from user information page

--- acceptance-tests/src/test/java/uk/gov/di/test/pages/UserInformationPage.java
+++ acceptance-tests/src/test/java/uk/gov/di/test/pages/UserInformationPage.java
@@ -6,10 +6,15 @@ import uk.gov.di.test.utils.Driver;
 public class UserInformationPage extends BasePage {
 
     By idTokenField = By.id("user-info-id-token");
+    By accessTokenField = By.id("user-info-access-token");
     By logoutButton = By.name("logout");
 
     public String getIdToken() {
         return Driver.get().findElement(idTokenField).getText();
+    }
+
+    public String getAccessToken() {
+        return Driver.get().findElement(accessTokenField).getText();
     }
 
     public void logoutOfAccount() {
