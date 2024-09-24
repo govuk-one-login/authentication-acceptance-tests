2d3a1f22c278dee570d249cc504b9584a2f3670a
Gabriel.Swallow <gabriel.swallow@digital.cabinet-office.gov.uk>
2024-09-24 14:57:51 +0100

    ATO-1013: add bsid cookie to OneLoginSession

--- acceptance-tests/src/test/java/uk/gov/di/test/utils/OneLoginSession.java
+++ acceptance-tests/src/test/java/uk/gov/di/test/utils/OneLoginSession.java
@@ -10,6 +10,7 @@ public class OneLoginSession {
     private String clientSessionID;
     private String persistentSessionId;
     private String languageFromCookie;
+    private String browserSessionId;
 
     public OneLoginSession(Set<Cookie> cookies) {
         for (Cookie cookie : cookies) {
@@ -25,6 +26,9 @@ public class OneLoginSession {
                 case "lng":
                     languageFromCookie = cookie.getValue();
                     break;
+                case "bsid":
+                    browserSessionId = cookie.getValue();
+                    break;
                 default:
                     break;
             }
@@ -37,6 +41,7 @@ public class OneLoginSession {
         json.put("clientSessionId", clientSessionID);
         json.put("persistentSessionId", persistentSessionId);
         json.put("languageFromCookie", languageFromCookie.toUpperCase());
+        json.put("browserSessionId", browserSessionId);
         return json;
     }
 }
