2eaf3b9ff59ddf043440a41dda359a03157e51c9
whi-tw <tom.whitwell@digital.cabinet-office.gov.uk>
2025-03-17 17:23:29 +0000

    BAU: Actually fix the problem (envars) (#615)
    All the iteration since cc3f929acfcf6c3809651364aad3995edfc279db
    has been unnecessary, except for sourcing `/test/.env`.
    
    This is actually all that's needed.

--- docker/run-acceptance-tests.sh
+++ docker/run-acceptance-tests.sh
@@ -8,5 +8,12 @@ if [ -z "${AWS_REGION:-}" ]; then
 fi
 
 pushd /test > /dev/null || exit 1
+
+if [ -f ".env" ]; then
+  # source .env file if it exists (as we're running in docker, it's definitely needed if it's there)
+  # shellcheck source=/dev/null
+  set -o allexport && source .env && set +o allexport
+fi
+
 ./gradlew --no-daemon cucumber
 popd > /dev/null || exit 1
