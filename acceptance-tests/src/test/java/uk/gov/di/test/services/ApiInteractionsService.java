2ddaf068baae7e052f28b3a98296fcb64363dd20
Andrew Moores <andrew.moores@digital.cabinet-office.gov.uk>
2025-03-19 15:08:56 +0000

    AUT-4072: Add ability to run API tests
    We need to write acceptance tests against the Account Management API
    starting with the new MFA Methods Management endpoint.  This repo has
    some hooks defined that are very specific for testing though the UI so
    the tests have been updated to either have a tag of @UI or @API so that
    the API tests can be excluded from the hooks.
    
    A new feature file has been added for the mfa-methods endpoint with a
    very simple call to the API to prove how to call API endpoints.
    
    The `openapi-specs` repo allows Imposter to be run to provide the
    example responses defined in the OpenAPI spec as used to prove the first
    acceptance test locally.  We need a mock server for the mfa-methods
    endpoint as this API is a private API and not easily accessed from a
    developers' laptop.

--- acceptance-tests/src/test/java/uk/gov/di/test/services/ApiInteractionsService.java
+++ acceptance-tests/src/test/java/uk/gov/di/test/services/ApiInteractionsService.java
@@ -19,33 +19,37 @@ public class ApiInteractionsService {
 
         LOG.debug("/authenticate requested");
 
-        var response = RestAssured.given()
-                .header("Authorization", "Bearer " + token)
-                .body(body)
-                .when()
-                .post("https://91ttse4tee.execute-api.eu-west-2.amazonaws.com/dev/authenticate");
+        var response =
+                RestAssured.given()
+                        .header("Authorization", "Bearer " + token)
+                        .body(body)
+                        .when()
+                        .post(
+                                "https://91ttse4tee.execute-api.eu-west-2.amazonaws.com/dev/authenticate");
 
         LOG.debug("/authenticate response: {}", response.asPrettyString());
     }
 
     public static void sendOtp(String token, String email, String phoneNumber) {
-        var body = """
+        var body =
+                """
                 {
                 "notificationType": "VERIFY_PHONE_NUMBER",
                 "email": "%s",
                 "phoneNumber": "%s"
                 }
                 """
-                .formatted(email, phoneNumber);
+                        .formatted(email, phoneNumber);
 
         LOG.debug("/send-otp-notification requested");
 
-        var response = RestAssured.given()
-                .header("Authorization", "Bearer " + token)
-                .body(body)
-                .when()
-                .post(
-                        "https://91ttse4tee.execute-api.eu-west-2.amazonaws.com/dev/send-otp-notification");
+        var response =
+                RestAssured.given()
+                        .header("Authorization", "Bearer " + token)
+                        .body(body)
+                        .when()
+                        .post(
+                                "https://91ttse4tee.execute-api.eu-west-2.amazonaws.com/dev/send-otp-notification");
 
         LOG.debug("/send-otp-notification response: {}", response.asPrettyString());
     }
