d72a099ef4c9cdc608cd460eb0d3aea1330869a6
Christian Sophocleous <145482922+ChristianSoftwire@users.noreply.github.com>
2026-03-30 15:20:37 +0100

    AUT-5212: Add acceptance tests for the create passkeys journey
    - Add test when user clicks success in stub, we expect them to be moved onto updated terms and conditions
    - Add test when user clicks skip in stub, we expect them to be moved onto updated terms and conditions
    - Add test when user clicks back in stub, we expect them to be taken back to the create-passkey page

--- acceptance-tests/src/test/java/uk/gov/di/test/step_definitions/PasskeysStepDef.java
+++ acceptance-tests/src/test/java/uk/gov/di/test/step_definitions/PasskeysStepDef.java
@@ -1,9 +1,11 @@
 package uk.gov.di.test.step_definitions;
 
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import uk.gov.di.test.pages.BasePage;
 import uk.gov.di.test.pages.CreatePasskeysPage;
 
-public class PasskeysStepDef {
+public class PasskeysStepDef extends BasePage {
 
     public CreatePasskeysPage createPasskeysPage;
 
@@ -14,5 +16,10 @@ public class PasskeysStepDef {
     @When("the user chooses to skip passkey registration")
     public void theUserChoosesToSkipPasskeyRegistration() {
         createPasskeysPage.clickSkipButton();
+    }
+
+    @Then("the user is taken to the AMC stub page passkey create page")
+    public void theUserIsTakenToTheAMCStubPagePasskeyCreatePage() {
+        waitForThisText("AMC stub (passkey create)");
     }
 }
